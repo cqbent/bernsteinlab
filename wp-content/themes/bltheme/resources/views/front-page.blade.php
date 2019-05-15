91e7f09fbdf161d43fd5c4bd814f98e0a2a5124c
cqbent <cqbent@gmail.com>
2019-05-15 10:23:39 -0400

    home page content and styling updates

--- wp-content/themes/bltheme/resources/views/front-page.blade.php
+++ wp-content/themes/bltheme/resources/views/front-page.blade.php
@@ -4,13 +4,31 @@
   @while(have_posts()) @php the_post() @endphp
     <div class="home-banner">
       @php (print get_the_post_thumbnail( get_the_ID(), 'full' ))
-      <div class="content">
-        <div class="tagline">
-          Epigenomic Regulation in Development and Cancer
-        </div>
-        <a href="/research" class="btn btn-primary">What We Do</a>
+      <div class="tagline">
+        Epigenomic Regulation in Development and Cancer
       </div>
-
+    </div>
+    <div class="home-content">
+      <div class="info row justify-content-center">
+        <div class="column col-sm-6">
+          <h4>What We Do</h4>
+          <p>
+            The Bernstein lab pursues a systems-level, molecular understanding of chromatin structure and
+            the epigenetic regulation of cellular state, and how these control systems go awry in diseases
+            like cancer.</p>
+          <a href="/research" class="btn btn-primary">More</a>
+        </div>
+        <div class="column col-sm-6">
+          <h4>Locations</h4>
+          <p><strong>MGH, Simches Research Center</strong><br />
+            185 Cambridge Street
+            Boston, MA</p>
+          <p><strong>Broad Institute</strong><br />
+            415 Main Street, Room 6041
+            Cambridge, MA</p>
+          <a href="/locations" class="btn btn-primary">More</a>
+        </div>
+      </div>
     </div>
     <div class="latest-news">
       <?php print bernstein_latest_news(); ?>
